f2b93615a3198af1b92bf9563a8d44a31e1e8125
Abid <abid.quereshi@outlook.com>
2026-06-21 23:34:32 +0300

    Broken Promises

---
+++ Mob/TestProject1.Tests/Test1.cs
@@ -0,0 +1,19 @@
+﻿namespace TestProject1.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void FalseIsTrue()
+        {
+
+            Assert.IsTrue(false);
+        }
+
+        public void TrueIsTrue()
+        {
+
+            Assert.IsTrue(true);
+        }
+    }
+}
