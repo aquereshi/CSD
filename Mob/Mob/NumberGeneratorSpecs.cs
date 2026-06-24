8dc6061125905382d2ac15b84bb3f7702441ee11
Abid <abid.quereshi@outlook.com>
2026-06-24 16:11:42 +0300

    Init

--- Mob/Mob/NumberGeneratorSpecs.cs
+++ Mob/Mob/NumberGeneratorSpecs.cs
@@ -4,70 +4,18 @@
     public sealed class NumberGeneratorSpecs
     {
         [TestMethod]
-        public void EvenNumber()
+        public void TestMethod1()
         {
             //Arrange
-            NumberGenerator numberGenerator = new NumberGenerator();
+       
 
             //Act
-            int number = numberGenerator.Generate();
+           
 
             //Assert
-            Assert.AreEqual(0, number % 2);
+           
         }
 
-        [TestMethod]
-        public void EndwithTwo()
-        {
-            //Arrange
-            NumberGenerator numberGenerator = new NumberGenerator();
-
-            //Act
-            int number = numberGenerator.Generate();
-
-            //Assert
-            Assert.AreEqual(2, number % 10);
-        }
-
-
-        [TestMethod]
-        public void GreaterThan9()
-        {
-            //Arrange
-            NumberGenerator numberGenerator = new NumberGenerator();
-
-            //Act
-            int number = numberGenerator.Generate();
-
-            //Assert
-            Assert.IsGreaterThan(9, number);
-        }
-
-
-        [TestMethod]
-        public void LessThan100()
-        {
-            //Arrange
-            NumberGenerator numberGenerator = new NumberGenerator();
-
-            //Act
-            int number = numberGenerator.Generate();
-
-            //Assert
-            Assert.IsLessThan(100, number);
-        }
-
-        [TestMethod]
-        public void SumOfBothDigitsEqualSix()
-        {
-            //Arrange
-            NumberGenerator numberGenerator = new NumberGenerator();
-
-            //Act
-            int number = numberGenerator.Generate();
-
-            //Assert
-            Assert.AreEqual(6, (number % 10) + (number / 10));
-        }
+     
     }
 }
