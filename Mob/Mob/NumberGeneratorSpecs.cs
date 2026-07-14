3916fb6b7ebbc70afbb4dbb57b8e155bc0400e80
Abid <abid.quereshi@outlook.com>
2026-07-14 08:53:44 +0100

    restart

--- Mob/Mob/NumberGeneratorSpecs.cs
+++ Mob/Mob/NumberGeneratorSpecs.cs
@@ -4,71 +4,16 @@
     public sealed class NumberGeneratorSpecs
     {
         [TestMethod]
-        public void IsEven()
+        public void TestMethod()
         {
             //Arrange
-            NumberGenerator numberGenerator = new NumberGenerator();
+   
 
             //Act
-            int number = numberGenerator.Generate();
+         
 
             //Assert
-            Assert.IsTrue(number % 2==0);           
-        }
-
-        [TestMethod]
-        public void EndWith2()
-        {
-            //Arrange
-            NumberGenerator numberGenerator = new NumberGenerator();
-
-            //Act
-            int number = numberGenerator.Generate();
-            bool endingWithTwo = number.ToString().EndsWith('2');
-
-            //Assert
-            Assert.IsTrue(endingWithTwo);
-        }
-
-        [TestMethod]
-        public void GreaterThan9()
-        {
-            //Arrange
-            NumberGenerator numberGenerator = new NumberGenerator();
-
-            //Act
-            int number = numberGenerator.Generate();
-            
-            //Assert
-            Assert.IsTrue(number > 9);
-
-        }
-
-        [TestMethod]
-        public void LessThan100()
-        {
-            //Arrange
-            NumberGenerator numberGenerator = new NumberGenerator();
-
-            //Act
-            int number = numberGenerator.Generate();
-
-            //Assert
-            Assert.IsTrue(number < 100);
-        }
-
-        [TestMethod]
-        public void SumOfBothDigitsIsSix()
-        {
-            //Arrange
-            int number = new NumberGenerator().Generate();
-
-            //Act
-            int firstDigit = number / 10;
-            int secondDigit = number % 10;
-
-            //Assert
-            Assert.IsTrue( firstDigit + secondDigit == 6);
+                   
         }
 
 
